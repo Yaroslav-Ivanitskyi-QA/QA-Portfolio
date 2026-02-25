8f41a0536cb2243973414072d2836697e5a7970f
Yaroslav <261805003+Yaroslav-Ivanitskyi-QA@users.noreply.github.com>
2026-02-25 23:20:49 +0100

    Add user test data

---
+++ demoblaze-full-testing-cycle/test-data/users.ts
@@ -0,0 +1,9 @@
+export const validUser={
+    username: 'TestUser!@#123',
+    password: 'Tester@!21'
+};
+
+export const invalidUser={
+    username: 'InvalidUser',
+    password: 'WrongPassword'
+};
